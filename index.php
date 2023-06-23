3878c33b4e2284d6dd3953ce342af115f1bc0746
EstyBrandes <95626359+EstyBrandes@users.noreply.github.com>
2023-06-23 16:24:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,44 @@
 
 <?php
-    include 'db.php';
-    include "config.php";
+    // Include database and configuration files
 
-    if(!empty($_POST["loginMail"])){
-        echo 'FROM SENT';
+    include 'config.php';
+
+    // Initialize the variables
+    $email = "";
+    $password = "";
+    $message = "";
+
+    // Check if the form is submitted
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Retrieve the email and password from the form
+        $email = $_POST["loginMail"];
+        $password = $_POST["loginPass"];
+
+        // Create a database connection
+        $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+        // Check the database connection
+        if (mysqli_connect_errno()) {
+            die("DB connection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . ")");
+        }
+
+        // Prepare the query to fetch the user with the given email and password
+        $query = "SELECT * FROM tbl_207_users WHERE email = '$email' AND password = '$password'";
+        $result = mysqli_query($connection, $query);
+
+        // Check if the query was successful
+        if ($result && mysqli_num_rows($result) > 0) {
+            // Redirect the user to "patients.php" page
+            header("Location: patients.php");
+            exit;
+        } else {
+            // Invalid credentials, display an error message
+            $message = "Invalid email or password";
+        }
+
+        // Close the database connection
+        mysqli_close($connection);
     }
 ?>
 
@@ -14,25 +48,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css">
     <script src="js/script.js" defer></script>
     <title>Login</title>
+</head>
 <body>
-<div class="container">
-           <h1>Login</h1>
-           <form action="#" method="post" id="frm">
-               <div class="form-group">
-                   <label for="loginMail">Email: </label>
-                   <input type="email" class="form-control" name="loginMail" id="loginMail" aria-describedby="emailHelp" placeholder="Enter email">
-               </div>
-               <div class="form-group">
-                   <label for="loginPass">Password: </label>
-                   <input type="password" class="form-control" name="loginPass" id="loginPass" placeholder="Enter Password">
-               </div>
-               <button type="submit" class="btn btn-primary">Log Me In</button>
-               <div class="error-message"><?php if(isset($message)) { echo $message; } ?></div>   
-          </form>
- 	   </div>
-
+    <div class="container"  class="form-group">
+        <h1>Login</h1>
+        <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post" id="frm">
+            <div class="form-group">
+                <label for="loginMail">Email: </label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="loginMail" id="loginMail" aria-describedby="emailHelp" placeholder="Enter email" required>
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label for="loginPass" for="exampleInputPassword1">Password: </label>
+                <input type="password" class="form-control" id="exampleInputPassword1" name="loginPass" id="loginPass" placeholder="Enter Password" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Log Me In</button>
+        </form>
+        <div class="error-message"><?php echo $message; ?></div>
+    </div>
 </body>
 </html>
+
